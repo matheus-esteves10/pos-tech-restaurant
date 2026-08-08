393acb03b7c922df2162fb5490a4f520821ac462
Andreas Santos <andreassantos.3003@gmail.com>
2026-08-08 18:33:47 -0300

    feat: Auth Controller

---
+++ src/main/java/br/com/fiap/restaurant/controller/AuthController.java
@@ -0,0 +1,28 @@
+package br.com.fiap.restaurant.controller;
+
+import br.com.fiap.restaurant.dto.request.LoginRequest;
+import br.com.fiap.restaurant.dto.response.LoginResponse;
+import br.com.fiap.restaurant.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+@Tag(name = "Auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @Operation(summary = "Authenticate a user and obtain a bearer token")
+    @PostMapping("/login")
+    public LoginResponse login(@Valid @RequestBody LoginRequest request) {
+        return authService.login(request);
+    }
+}
